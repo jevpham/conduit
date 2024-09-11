6386dfdb658357d184107ca6eef391a2ce2a7e7f
Jeff <gggpham@gmail.com>
2024-09-11 09:54:12 -0700

    rerun

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,13 +144,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [ANGULAR_DIR]
 
 # For development only
-if os.environ.get('VERCEL_ENV') != 'production':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if os.environ.get('VERCEL_ENV') != 'production':
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
