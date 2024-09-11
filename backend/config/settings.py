977f634c30fd3a99447e790ab5b9924d00c87956
Jeff <gggpham@gmail.com>
2024-09-11 09:35:42 -0700

    vercel hosting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 # Authentication
 AUTH_USER_MODEL = "users.User"
